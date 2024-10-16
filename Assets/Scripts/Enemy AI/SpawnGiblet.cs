ee0f61a747b9e27b118f2e3c43a52686c59cf6d1
Aiden Thacker <aidenthacker86@gmail.com>
2024-10-16 10:37:15 -0500

    Made QR code and Finished Giblets

--- Assets/Scripts/Enemy AI/SpawnGiblet.cs
+++ Assets/Scripts/Enemy AI/SpawnGiblet.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class SpawnGiblet : MonoBehaviour
 {
     public GameObject blood;
+    public GameObject deathBlood;
     public Transform bleedLocation;
+    public Transform dieLocation;
 
     public void BleedingOut()
     {
         GameObject gib = Instantiate(blood, bleedLocation.position, Quaternion.identity);
-        //Destroy(gib, 1.0f);
+        Destroy(gib, 1.0f);
+    }
+
+    public void Died()
+    {
+        GameObject gib = Instantiate(deathBlood, dieLocation.position, Quaternion.identity);
+        Destroy(gib, 1.0f);
     }
 }
